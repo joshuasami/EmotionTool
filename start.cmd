ce0ab27f15688f6207d360d61f476339c95eb231
Berit <peterlustig@haha.de>
2022-08-24 22:32:42 +0200

    Berit Wortliste

--- start.cmd
+++ start.cmd
@@ -1,2 +1,2 @@
-Python-Portable-3.9.6\apps\python main.py
+python main.py
 pause
--- template_settings.py
+++ template_settings.py
@@ -26,3 +26,35 @@ coder = "JSB"
 
 # Spalten, die mit angezeit werden sollen
 showLabels = ["vignette", "Bemerkung", "gender"]
+
+
+****für Erwachsene folgendes in Settings ändern:
+
+# input-File
+file = "in/finalTest.csv"
+
+# Listen
+nonwordsUrl = "lists/nonwords.csv"
+negationsUrl = "lists/negation.csv"
+modifikatorUrl = "lists/modifikator.csv"
+
+# ET = output-File , EC = input/output-File
+outFile = "out/testout.csv"
+
+# Spalten, die in der Auswettung untersucht werden
+labels = ['Emotionswort']
+
+# Fragt bei True bei Funden in der ersten Spalte immer nach. Wenn nicht gewünscht --> False
+firstIgnore = False
+
+# Optionen: "de" und "en"
+language = "de"
+
+
+#### E.C. ####
+
+# Kodierer
+coder = "BS"
+
+# Spalten, die mit angezeit werden sollen
+showLabels = ["CASE (Interviewnr.)", "vignette"]
